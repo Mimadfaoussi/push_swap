59f1897b5631391287ebf3299247c48ef4f246ac
mfaoussi <imadfaoussi@gmail.com>
2024-04-10 06:13:20 +0200

    update

--- init.c
+++ init.c
@@ -1,24 +1,47 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   sorts.c                                            :+:      :+:    :+:   */
+/*   init.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: mfaoussi <mfaoussi@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/18 00:52:11 by mfaoussi          #+#    #+#             */
-/*   Updated: 2024/04/08 19:44:38 by mfaoussi         ###   ########.fr       */
+/*   Updated: 2024/04/10 05:21:37 by mfaoussi         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void    preparing_a(t_node **a, t_node **b)
+void	preparing_a(t_node **a, t_node **b)
 {
 	index_position(a);
 	index_position(b);
 	a_targets(a, b);
 	set_cost_a(a, b);
 	set_cheapest(a);
+}
+
+void	index_position(t_node **stack)
+{
+	t_node	*index;
+	int		i;
+	int		middle;
+
+	if (!*stack)
+		return ;
+	index = *stack;
+	middle = stack_length(*stack) / 2;
+	i = 0;
+	while (index)
+	{
+		if (i <= middle)
+			index->above_median = true;
+		else
+			index->above_median = false;
+		index->index = i;
+		index = index->next;
+		i++;
+	}
 }
 
 /**
@@ -55,42 +78,7 @@ void	a_targets(t_node **a, t_node **b)
 	}
 }
 
-// t_node	*stack_max_node(t_node **stack)
-// {
-// 	t_node	*max;
-// 	t_node	*index;
-
-// 	index = *stack;
-// 	max = *stack;
-// 	while (index)
-// 	{
-// 		if (index->value > max->value)
-// 			max = index;
-// 		index = index->next;
-// 	}
-// 	return (max);
-// }
-
-void    index_position(t_node **stack)
+void	set_cost_a(t_node **a, t_node **b)
 {
-	t_node  *index;
-	int     i;
-	int     middle;
-
-	if (!*stack)
-		return ;
-	index = *stack;
-	middle = stack_length(*stack) / 2;
-	i = 0;
-	while (index)
-	{
-		if (i <= middle)
-			index->above_median = true;
-		else
-			index->above_median = false;
-		index->index = i;
-		index = index->next;
-		i++;
-	}
+	
 }
-
